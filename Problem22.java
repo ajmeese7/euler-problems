5a4d927b19f3f07331ef6df9df313d4da077f26d
Aaron Meese <ajmeese7@gmail.com>
2018-06-13 14:34:14 -0500

    Solved Problem 22
    Thanks for reminding me about these problems, Cyril!

--- Problem22.java
+++ Problem22.java
@@ -6,15 +6,21 @@
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 
 public class Problem22 {
 	static HashMap<Character, Integer> values = new HashMap<>();
+	public static String fileName = "Problem22Text.txt";
+	static ArrayList<String> names = new ArrayList<>();
 	public static void main(String[] args) {
 		// to calculate runtime of different methods
 		double startTime = System.nanoTime();
 		
 		
+		// Text file is all in uppercase
 		String temp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 		char alphabet[] = temp.toCharArray();
 		
@@ -25,23 +31,16 @@ public class Problem22 {
 		}
 		
 		long totalScore = 0;
+		int rank = 1;
 		try {
-			BufferedReader scanner = new BufferedReader(new FileReader("Problem22Text.txt"));
-			String line = "";
-			int rank = 1;
+			organizeNames();
+			Collections.sort(names);
 			
-	        while ((line = scanner.readLine()) != null) {
-	        	// Splits based on where the comma separates the names
-	            String[] namesList = line.split(",");
-	
-	            for (String name : namesList) {
-	            	// Removes double quotes
-	                name = name.substring(1, name.length() - 1);
-	                totalScore += getNameValue(name, rank);
-	                rank++;
-	            }
-	        }
-	        scanner.close();
+			for (String name : names) {
+                totalScore += getNameValue(name, rank);
+                rank++;
+            }
+			
 		} catch (Exception ex) {
 			System.out.println("There was an error: " + ex);
 		}
@@ -62,5 +61,25 @@ public class Problem22 {
 		}
 		
 		return (score * rank);
+	}
+	
+	// Alphabetically orders the names
+	public static void organizeNames() {
+		try {
+			BufferedReader scanner = new BufferedReader(new FileReader(fileName));
+			String line = scanner.readLine();
+
+        	// Splits based on where the comma separates the names
+            String[] namesList = line.split(",");
+            for (String name : namesList) {
+            	// Removes double quotes
+                name = name.substring(1, name.length() - 1);
+                names.add(name);
+            }
+
+	        scanner.close();
+		} catch (Exception ex) {
+			System.out.println(ex);
+		}
 	}
 }
