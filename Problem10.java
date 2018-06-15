f55d03389cc49ea565472ededaf3691bd327d3fc
Aaron Meese <ajmeese7@gmail.com>
2018-06-14 18:00:22 -0600

    Sped up problem 10
    Now it doesn't check over every even number, thereby cutting the runtime basically in half

--- Problem10.java
+++ Problem10.java
@@ -2,8 +2,6 @@
  * File: Problem10.java
  * ----------------------
  * Finds the sum of all primes below NUM.
- * 
- * Still needs some improvements as is takes over 3 seconds.
  */
 
 public class Problem10 {
@@ -23,7 +21,6 @@ public class Problem10 {
 		boolean status = true;
 		
 		for (int divisor = 2; divisor <= Math.sqrt(number); divisor++) {
-			
 			if (number % divisor == 0) {
 				status = false;
 				break;
@@ -36,13 +33,12 @@ public class Problem10 {
 	public static long getPrimeSum(int n) {
 		long sum = 0;
 		
-		for (int number = 2; number < n; number++) {
-			
+		for (int number = 3; number < n; number += 2) {
 			if (isPrime(number)) {
 				sum += number;
 			}
 		}
 		
-		return sum;
+		return sum + 2; // Because 2 is prime
 	}
 }
