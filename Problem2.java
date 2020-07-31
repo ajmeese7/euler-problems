e886bbd288ed456c2ae2bba348a391fc2e027b55
Aaron Meese <ajmeese7@gmail.com>
2020-07-31 14:30:19 -0500

    Update Problem2.java for HackerRank
    I'm gonna start trying to do the more challenging versions every now and then, just for shits and giggles.

--- Problem2.java
+++ Problem2.java
@@ -1,30 +1,32 @@
-// Finds the sum of all even Fibonacci terms below MAX_NUMBER
+// https://www.hackerrank.com/contests/projecteuler/challenges/euler002/problem
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
 public class Problem2 {
-	public static final int MAX_NUMBER = 4000000;
+    public static void main (String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for(int a0 = 0; a0 < t; a0++){
+            long maxNum = in.nextLong();
+            long num = 1;
+            long previousNum = 0;
+            long total = 0;
 
-	public static void main (String[] args) {
-		// to calculate runtime of different methods
-		double startTime = System.nanoTime();
-				
-		int n1 = 0;
-		int n2 = 1;
-		int n3 = 0;
-		int total = 0;
+            while (num < maxNum) {
+                if (num % 2 == 0) {
+                    total += num;
+                }
+                
+                long temp = previousNum;
+                previousNum = num;
+                num += temp;
+            }
 
-		while (n3 < MAX_NUMBER) {
-			if (n3 % 2 == 0) {
-				total += n3;
-			}
-			
-			n3 = n1 + n2;
-			n1 = n2;
-			n2 = n3;
-		}    
-
-		System.out.println(total);
-		
-		double duration = (System.nanoTime() - startTime) / 1000000000;
-		System.out.println(duration + " seconds");
-	}
+            System.out.println(total);
+        }
+    }
 }
