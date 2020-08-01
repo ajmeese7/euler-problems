6ce9dcf3b4dbf912fadc8293f079bc7ce4177016
Aaron Meese <ajmeese7@gmail.com>
2020-08-01 10:06:47 -0500

    Update Problem3.java for HackerRank
    Leaving the comment on this one because it makes sense and is a bit cleaner. Might even go back and add something similar to the other problems.

--- Problem3.java
+++ Problem3.java
@@ -6,53 +6,52 @@
  * Uses the logic that every number n can have at 
  * most one prime factor greater than sqrt(n).
  * 
+ * HackerRank link:
+ * https://www.hackerrank.com/contests/projecteuler/challenges/euler003/problem
  */
 
-public class Problem3 {
-	public static final long NUMBER = 600851475143L;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
-	public static void main (String[] args) {
-		// to calculate runtime of different methods
-		double startTime = System.nanoTime();
-				
-		long n = NUMBER;
-		int factor = 2;
-		int lastFactor = 1;
-		
-		if (n % 2 == 0) {
-			lastFactor = 2;
-			n = n / 2;
-			
-			while (n % 2 == 0) {
-				n = n / 2;
-			}
-		}
-		
-		factor = 3;
-		double maxFactor = Math.sqrt(n);
-		
-		while (n > 1 && factor <= maxFactor) {
-			if (n % factor == 0) {
-				n = n / factor;
-				lastFactor = factor;
-				
-				while (n % factor == 0) {
-					n = n / factor;
-				}
-				
-				maxFactor = Math.sqrt(n);
-			}
-			
-			factor = factor + 2;
-		}
-		
-		if (n == 1) {
-			System.out.println(lastFactor);
-		} else {
-			System.out.println(n);
-		}
-		
-		double duration = (System.nanoTime() - startTime) / 1000000000;
-		System.out.println(duration + " seconds");
-	}
+public class Problem3 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for(int a0 = 0; a0 < t; a0++){
+            long n = in.nextLong();
+            int factor = 2;
+            int lastFactor = 1;
+            
+            if (n % 2 == 0) {
+                lastFactor = 2;
+                n /= 2;
+                
+                while (n % 2 == 0) {
+                    n /= 2;
+                }
+            }
+            
+            factor = 3;
+            double maxFactor = Math.sqrt(n);
+            while (n > 1 && factor <= maxFactor) {
+                if (n % factor == 0) {
+                    n /= factor;
+                    lastFactor = factor;
+                    
+                    while (n % factor == 0) {
+                        n /= factor;
+                    }
+                    
+                    maxFactor = Math.sqrt(n);
+                }
+                
+                factor += 2;
+            }
+            
+            System.out.println(n == 1 ? lastFactor : n);
+        }
+    }
 }
