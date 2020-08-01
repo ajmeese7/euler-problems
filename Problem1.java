0bf7fcbd331acfb38d3752b930f3c5855e69db4b
Aaron Meese <ajmeese7@gmail.com>
2020-08-01 09:59:52 -0500

    Update Problem1.java for HackerRank
    I'm still pretty rusty, so this one took me a lot of Googling to figure out where I was going wrong. I was trying to use BigInteger and stuff when it wasn't necessary and over-complicating the problem.

--- Problem1.java
+++ Problem1.java
@@ -1,19 +1,37 @@
-// Finds the sum of all multiples of NUM_ONE or NUM_TWO below MAX_NUMBER
+// https://www.hackerrank.com/contests/projecteuler/challenges/euler001/problem
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
 public class Problem1 {
-	public static final int NUM_ONE = 3;
-	public static final int NUM_TWO = 5;
-	public static final int MAX_NUMBER = 1000;
-	
-	public static void main (String[] args) {
-		// I made it a long to help if MAX_NUMBER is larger. Unsure if working
-		long total = 0L;
-		for (int i = 0; i < MAX_NUMBER; i++) {
-			if (i % NUM_ONE == 0 || i % NUM_TWO == 0) {
-				total += i;
-			}
-		}
-		
-		System.out.println(total);
-	}
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        long t = in.nextLong();
+        for (int a0 = 0; a0 < t; a0++) {
+            long n = in.nextLong();
+            long a = 0, b = 0, c = 0;
+
+            // (n-1) because sum BELOW n, %3 removes remainder
+            // on the next line;
+            // /3 gets # of 3 multiples
+            a = (n-1) % 3;
+            a = n - 1 - a;
+            a = a / 3;
+
+            b = (n-1) % 5;
+            b = n - 1 - b;
+            b = b / 5;
+
+            c = (n-1) % 15;
+            c = n - 1 - c;
+            c = c / 15;
+            
+            // Add formulae of each segment and subtract duplicates
+            long total = 3*a*(a+1)/2 + 5*b*(b+1)/2 - 15*c*(c+1)/2;
+            System.out.println(total);
+        }
+    }
 }
