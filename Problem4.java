b13b694b56d1cddeae1acb432db91e4904b8f87f
Aaron Meese <ajmeese7@gmail.com>
2020-08-01 10:15:21 -0500

    Update Problem4.java for HackerRank
    Super simple change on this one.

--- Problem4.java
+++ Problem4.java
@@ -1,47 +1,53 @@
-// Finds the largest palindrome made from the product of two NUM-digit numbers
+// https://www.hackerrank.com/contests/projecteuler/challenges/euler004/problem
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
 public class Problem4 {
-	public static final int NUM = 3;
+    public static final int NUM = 3;
 
-	public static void main (String[] args) {
-		// to calculate runtime of different methods
-		double startTime = System.nanoTime();
-		
-		String nines = "9";
-		String small = "1";
-		for (int i = 1; i < NUM; i++) {
-			nines += "9";
-			small += "0";
-		}
-		
-		int numOne = Integer.parseInt(nines);
-		int numTwo = numOne;
-		int minNum = Integer.parseInt(small);
-		int largestPalindrome = 0;
-		
-		while (numOne >= minNum) {
-			while (numTwo >= minNum) {
-				int temp = numOne * numTwo;
-				if (isPalindrome(temp) && temp > largestPalindrome) {
-					largestPalindrome = temp;
-				} else if (temp <= largestPalindrome) {
-					break;
-				}
-				
-				numTwo--;
-			}
-			
-			numOne--;
-			numTwo = numOne;
-		}
-		
-		System.out.println(largestPalindrome);
-		
-		double duration = (System.nanoTime() - startTime) / 1000000000;
-		System.out.println(duration + " seconds");
-	}
-	
-	public static boolean isPalindrome(int number) {
-		return String.valueOf(number).equals(new StringBuilder(String.valueOf(number)).reverse().toString());
-	}
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for(int a0 = 0; a0 < t; a0++){
+            int n = in.nextInt();
+            String nines = "9";
+            String small = "1";
+            for (int i = 1; i < NUM; i++) {
+                nines += "9";
+                small += "0";
+            }
+            
+            int numOne = Integer.parseInt(nines);
+            int numTwo = numOne;
+            int minNum = Integer.parseInt(small);
+            int largestPalindrome = 0;
+            
+            while (numOne >= minNum) {
+                while (numTwo >= minNum) {
+                    int temp = numOne * numTwo;
+                    if (isPalindrome(temp) && temp > largestPalindrome && temp < n) {
+                        largestPalindrome = temp;
+                    } else if (temp <= largestPalindrome) {
+                        break;
+                    }
+                    
+                    numTwo--;
+                }
+                
+                numOne--;
+                numTwo = numOne;
+            }
+            
+            System.out.println(largestPalindrome);
+        }
+    }
+
+    
+    public static boolean isPalindrome(int number) {
+        return String.valueOf(number).equals(new StringBuilder(String.valueOf(number)).reverse().toString());
+    }
 }
