f6d9c93300a350684edde851a78f40b24a6dc2df
Aaron Meese <ajmeese7@gmail.com>
2020-08-01 10:20:00 -0500

    Update Problem5.java
    Forgot to change the name when I copied my solution over. Oopsie.

--- Problem5.java
+++ Problem5.java
@@ -17,12 +17,11 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class Solution {
-
+public class Problem5 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int t = in.nextInt();
-        for(int a0 = 0; a0 < t; a0++){
+        for (int a0 = 0; a0 < t; a0++) {
             int n = in.nextInt();
             System.out.println(LCM(n));
         }
